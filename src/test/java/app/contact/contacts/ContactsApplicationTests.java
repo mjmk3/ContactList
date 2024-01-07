9d4eb2683e14ed0d166e51be0021344175481694
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-07 23:39:27 +0300

    Starting contacts

---
+++ src/test/java/app/contact/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.contact.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
