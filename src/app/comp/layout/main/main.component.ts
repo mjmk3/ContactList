ec00c2ac34a7fe2db533d299e1a79f618c046dbc
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-08 13:00:11 +0300

    PrimeNg

--- src/app/comp/layout/main/main.component.ts
+++ src/app/comp/layout/main/main.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../header/header.component";
+import {FooterComponent} from "../footer/footer.component";
+import {HomeComponent} from "../home/home.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
