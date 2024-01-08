bc65a6941fb0aa860105237d4063745f726f4aa9
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-08 12:30:31 +0300

    PrimeNg

---
+++ src/app/comp/contact/contact-main/contact-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-main.component.html',
+  styleUrl: './contact-main.component.css'
+})
+export class ContactMainComponent {
+
+}
---
+++ src/app/comp/contact/contact-profile/contact-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-profile.component.html',
+  styleUrl: './contact-profile.component.css'
+})
+export class ContactProfileComponent {
+
+}
